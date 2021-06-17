4f997157cb8c33d6167a9742c8f17ed696c9a94d
Sergey <zaymovskey@mail.ru>
2021-06-17 14:32:24 +0300

    Status fix

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,9 +1,13 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 
 const ProfileStatus = (props) => {
     const [editMode, setEditMode] = useState(false);
     const [status, setStatus] = useState(props.status);
+
+    useEffect(() => {
+        setStatus(props.status)
+    });
 
     const onStatusChange = (e) => {
         setStatus(e.target.value)
